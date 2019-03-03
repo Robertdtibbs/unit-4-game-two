5022594c1e654dcd2313ef9b9a5c720b6798591c
Teb Mosby <tibbsr09@gmail.com>
2019-03-03 16:40:17 -0500

    completed styling and started JS and JQ

---
+++ assests/javascript/game.js
@@ -0,0 +1,7 @@
+var fankBeer = Math.floor(Math.random()*11)+1;
+var deeBeer = Math.floor(Math.random()*11)+1;
+var macBeer = Math.floor(Math.random()*11)+1;
+var dennisBeer = Math.floor(Math.random()*11)+1;
+var charlieBeer = Math.floor(Math.random()*11)+1;
+var beerNumber = Math.floor(Math.random()*71)+31;
+
